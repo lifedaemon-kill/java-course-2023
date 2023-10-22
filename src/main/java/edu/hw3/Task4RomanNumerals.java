99e2748a819df0fb2f1242878b6590ad4291ff9b
Ruslan <noname0263@gmail.com>
2023-10-23 00:08:59 +0700

    update Task4 algorithm and Test4

--- src/main/java/edu/hw3/Task4RomanNumerals.java
+++ src/main/java/edu/hw3/Task4RomanNumerals.java
@@ -6,37 +6,40 @@ final class Task4RomanNumerals {
     private Task4RomanNumerals() {
     }
 
-    private static final int LOWEST_BASE_MINUS_SUB_BASE = 4;
-
     //if last base star with "1" it should be 4, if with 5 then require different formula
     private static final int HIGHEST_VALUE_FOR_FINDING_MAX_POSSIBLE = 4;
 
     public static final int[] BASE_NUMBER = {
-        1,
-        5,
-        10,
-        50,
-        100,
-        500,
-        1000,
-        5000,
-        10000,
-        50000,
+        1, 4, 5, 9,
+        10, 40, 50, 90,
+        100, 400, 500, 900,
+        1000, 4000, 5000, 9000,
+        10000, 40000, 50000, 90000,
         100000
     };
     @SuppressWarnings("MagicNumber")
     public static final HashMap<Integer, String> MAP_BASE_NUMBER_TO_SYMBOL = new HashMap<>() {{
-        put(BASE_NUMBER[0], "Ⅰ");
-        put(BASE_NUMBER[1], "Ⅴ");
-        put(BASE_NUMBER[2], "Ⅹ");
-        put(BASE_NUMBER[3], "Ⅼ");
-        put(BASE_NUMBER[4], "Ⅽ");
-        put(BASE_NUMBER[5], "Ⅾ");
-        put(BASE_NUMBER[6], "ↀ");
-        put(BASE_NUMBER[7], "ↁ");
-        put(BASE_NUMBER[8], "ↂ");
-        put(BASE_NUMBER[9], "ↇ");
-        put(BASE_NUMBER[10], "ↈ");
+        put(BASE_NUMBER[0], "I");
+        put(BASE_NUMBER[1], "IV");
+        put(BASE_NUMBER[2], "V");
+        put(BASE_NUMBER[3], "IX");
+        put(BASE_NUMBER[4], "X");
+        put(BASE_NUMBER[5], "XL");
+        put(BASE_NUMBER[6], "L");
+        put(BASE_NUMBER[7], "XC");
+        put(BASE_NUMBER[8], "C");
+        put(BASE_NUMBER[9], "CD");
+        put(BASE_NUMBER[10], "D");
+        put(BASE_NUMBER[11], "Cↀ");
+        put(BASE_NUMBER[12], "ↀ");
+        put(BASE_NUMBER[13], "ↀↁ");
+        put(BASE_NUMBER[14], "ↁ");
+        put(BASE_NUMBER[15], "ↀↂ");
+        put(BASE_NUMBER[16], "ↂ");
+        put(BASE_NUMBER[17], "ↂↇ");
+        put(BASE_NUMBER[18], "ↇ");
+        put(BASE_NUMBER[19], "ↂↈ");
+        put(BASE_NUMBER[20], "ↈ");
     }};
 
     public static String convertToRoman(int originValue) throws Exception {
@@ -44,34 +47,15 @@ final class Task4RomanNumerals {
 
         StringBuilder result = new StringBuilder();
 
-        for (int i = BASE_NUMBER.length - 1; i >= 0; i--) {
-            if (value == 0) {
-                break;
+        int i = BASE_NUMBER.length - 1;
+        while (value > 0) {
+            while (value >= BASE_NUMBER[i]) {
+                value -= BASE_NUMBER[i];
+                result.append(MAP_BASE_NUMBER_TO_SYMBOL.get(BASE_NUMBER[i]));
             }
-
-            int base = BASE_NUMBER[i];
-            int subBase = 0;
-
-            if (value >= LOWEST_BASE_MINUS_SUB_BASE) {
-                subBase = (i % 2 == 0) ? BASE_NUMBER[i - 2] : BASE_NUMBER[i - 1];
-            }
-
-            if (value < base && value < base - subBase) {
-                continue;
-            }
-
-            // base 1 5 10 50 100 500 1000 etc
-            while (value >= base) {
-                value -= base;
-                result.append(MAP_BASE_NUMBER_TO_SYMBOL.get(base));
-            }
-            //base 4 9 40 90 etc
-            if (value >= base - subBase) {
-                value -= (base - subBase);
-                result.append(MAP_BASE_NUMBER_TO_SYMBOL.get(subBase));
-                result.append(MAP_BASE_NUMBER_TO_SYMBOL.get(base));
-            }
+            i--;
         }
+
         return result.toString();
     }
 
@@ -80,10 +64,8 @@ final class Task4RomanNumerals {
             throw new Exception("Value < 1");
         }
         if (BASE_NUMBER.length != MAP_BASE_NUMBER_TO_SYMBOL.size()) {
-            throw new Exception("BASE_NUMBER length != MAP length");
-        }
-        if (!String.valueOf(originValue).equals("1")) {
-            throw new Exception("LAST VALUE IN BASE_NUMBER AND MAP MUST START WITH 1");
+            throw new Exception(
+                "BASE_NUMBER length != MAP length :" + BASE_NUMBER.length + " != " + MAP_BASE_NUMBER_TO_SYMBOL.size());
         }
         if (originValue > BASE_NUMBER[BASE_NUMBER.length - 1] * HIGHEST_VALUE_FOR_FINDING_MAX_POSSIBLE - 1) {
             throw new Exception("Value is too high for converting");
--- src/test/java/edu/hw3/Task4RomanNumeralsTest.java
+++ src/test/java/edu/hw3/Task4RomanNumeralsTest.java
@@ -12,83 +12,83 @@ public class Task4RomanNumeralsTest {
     void test1() throws Exception {
         int arabic = 2;
         String roman = convertToRoman(arabic);
-        assertThat(roman).isEqualTo("ⅠⅠ");
+        assertThat(roman).isEqualTo("II");
     }
     @Test
     @DisplayName("2 Тест с курсa")
     void test2() throws Exception {
         int arabic = 12;
         String roman = convertToRoman(arabic);
-        assertThat(roman).isEqualTo("ⅩⅠⅠ");
+        assertThat(roman).isEqualTo("XII");
     }
     @Test
     @DisplayName("3 Тест с курсa")
     void test3() throws Exception {
         int arabic = 16;
         String roman = convertToRoman(arabic);
-        assertThat(roman).isEqualTo("ⅩⅤⅠ");
+        assertThat(roman).isEqualTo("XVI");
     }
     @Test
     @DisplayName("37_330")
     void test4() throws Exception {
         int arabic = 37_330;
         String roman = convertToRoman(arabic);
-        assertThat(roman).isEqualTo("ↂↂↂↁↀↀⅭⅭⅭⅩⅩⅩ");
+        assertThat(roman).isEqualTo("ↂↂↂↁↀↀCCCXXX");
     }
     @Test
     @DisplayName("99_999 (maxBase - 1)")
     void test5() throws Exception {
         int arabic = 99_999;
         String roman = convertToRoman(arabic);
-        assertThat(roman).isEqualTo("ↂↈↀↂⅭↀⅩⅭⅠⅩ");
+        assertThat(roman).isEqualTo("ↂↈↀↂCↀXCIX");
     }
     @Test
     @DisplayName("399_999 (the true max)")
     void test6() throws Exception {
         int arabic = 399_999;
         String roman = convertToRoman(arabic);
-        assertThat(roman).isEqualTo("ↈↈↈↂↈↀↂⅭↀⅩⅭⅠⅩ");
+        assertThat(roman).isEqualTo("ↈↈↈↂↈↀↂCↀXCIX");
     }
     @Test
     @DisplayName("1994")
     void test7() throws Exception {
         int arabic = 1994;
         String roman = convertToRoman(arabic);
-        assertThat(roman).isEqualTo("ↀⅭↀⅩⅭⅠⅤ");
+        assertThat(roman).isEqualTo("ↀCↀXCIV");
     }
     @Test
     @DisplayName("73")
     void test8() throws Exception {
         int arabic = 73;
         String roman = convertToRoman(arabic);
-        assertThat(roman).isEqualTo("ⅬⅩⅩⅠⅠⅠ");
+        assertThat(roman).isEqualTo("LXXIII");
     }
     @Test
     @DisplayName("96")
     void test9() throws Exception {
         int arabic = 96;
         String roman = convertToRoman(arabic);
-        assertThat(roman).isEqualTo("ⅩⅭⅤⅠ");
+        assertThat(roman).isEqualTo("XCVI");
     }
     @Test
     @DisplayName("271_386")
     void test10() throws Exception {
         int arabic = 271_386;
         String roman = convertToRoman(arabic);
-        assertThat(roman).isEqualTo("ↈↈↇↂↂↀⅭⅭⅭⅬⅩⅩⅩⅤⅠ");
+        assertThat(roman).isEqualTo("ↈↈↇↂↂↀCCCLXXXVI");
     }
     @Test
     @DisplayName("26_644")
     void test11() throws Exception {
         int arabic = 26644;
         String roman = convertToRoman(arabic);
-        assertThat(roman).isEqualTo("ↂↂↁↀⅮⅭⅩⅬⅠⅤ");
+        assertThat(roman).isEqualTo("ↂↂↁↀDCXLIV");
     }
     @Test
     @DisplayName("141_157")
     void test12() throws Exception {
         int arabic = 141157;
         String roman = convertToRoman(arabic);
-        assertThat(roman).isEqualTo("ↈↂↇↀⅭⅬⅤⅠⅠ");
+        assertThat(roman).isEqualTo("ↈↂↇↀCLVII");
     }
 }
