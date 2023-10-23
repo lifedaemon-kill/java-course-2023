b195b79de7d6024574ac50ce58b48f34e3cdc06a
Ruslan <noname0263@gmail.com>
2023-10-23 15:01:43 +0700

    update Task5

--- src/main/java/edu/hw3/Task5ContactList.java
+++ src/main/java/edu/hw3/Task5ContactList.java
@@ -1,5 +1,100 @@
 package edu.hw3;
 
 final class Task5ContactList {
-    private Task5ContactList(){}
+    private Task5ContactList() {
+    }
+
+    private static int parseInit(String sortOrder) throws Exception {
+        if (sortOrder.equals("DESC")) {
+            return -1;
+        } else if (sortOrder.equals("ABS")) {
+            return 1;
+        } else {
+            throw new Exception("The sorting type is incorrect");
+        }
+    }
+
+    public static Person[] parseContacts(String[] names, String sortOrder) throws Exception {
+        int orderType = parseInit(sortOrder);
+        if (names == null) {
+            return new Person[0];
+        }
+        Person[] sortArr = new Person[names.length];
+        for (int i = 0; i < names.length; i++) {
+            sortArr[i] = new Person(names[i]);
+        }
+
+        for (int i = 0; i < sortArr.length; i++) {
+            int pos = i;
+            Person prime = sortArr[i];
+            //цикл выбора наименьшего элемента
+            for (int j = i + 1; j < sortArr.length; j++) {
+                if (sortArr[j].compareTo(prime) * orderType < 0) {
+                    //pos - индекс наименьшего элемента
+                    pos = j;
+                    prime = sortArr[j];
+                }
+            }
+            sortArr[pos] = sortArr[i];
+            //меняем местами наименьший с sortArr[i]
+            sortArr[i] = prime;
+        }
+        return sortArr;
+    }
+
+    public static final class Person {
+        private final String firstName;
+        private  String lastName = null;
+
+        Person(String fullName) throws Exception {
+            String[] splitName = fullName.split(" ");
+            if (splitName.length != 1 && splitName.length != 2) {
+                throw new Exception("Name \"" + fullName + "\" is wrong");
+            }
+            this.firstName = splitName[0];
+            if (splitName.length == 2) {
+                this.lastName = splitName[1];
+            }
+        }
+
+        @Override
+        public String toString() {
+            if (lastName != null) {
+                return firstName + " " + lastName;
+            } else {
+                return firstName;
+            }
+        }
+
+        private String toReverseString() {
+            if (lastName != null) {
+                return lastName + firstName;
+            } else {
+                return firstName;
+            }
+        }
+
+        public int compareTo(Person anotherPerson) {
+            return this.toReverseString().compareTo(anotherPerson.toReverseString());
+        }
+
+        @Override
+        public boolean equals(Object another) {
+            if (this == another) {
+                return true;
+            }
+            if (another == null) {
+                return false;
+            }
+            if (!(another instanceof Person obj)) {
+                return false;
+            }
+            return this.toString().equals(obj.toString());
+        }
+
+        @Override
+        public int hashCode() {
+            return this.toString().hashCode();
+        }
+    }
 }
--- src/test/java/edu/hw3/Task5ContactListTest.java
+++ src/test/java/edu/hw3/Task5ContactListTest.java
@@ -1,4 +1,84 @@
 package edu.hw3;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static edu.hw3.Task5ContactList.Person;
+import static edu.hw3.Task5ContactList.parseContacts;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 public class Task5ContactListTest {
+    @Test
+    @DisplayName("1 Тест с курсa")
+    void test1() throws Exception {
+        String[] names = {"John Locke", "Thomas Aquinas", "David Hume", "Rene Descartes"};
+        Person[] expected = {
+            new Person("Thomas Aquinas"),
+            new Person("Rene Descartes"),
+            new Person("David Hume"),
+            new Person("John Locke")};
+
+        Person[] sorted = parseContacts(names, "ABS");
+        assertArrayEquals(expected, sorted);
+    }
+
+    @Test
+    @DisplayName("1 Тест с курсa, обратный ход")
+    void test2() throws Exception {
+        String[] arrayNames = {"John Locke", "Thomas Aquinas", "David Hume", "Rene Descartes"};
+        Person[] expectedNames = {
+            new Person("John Locke"),
+            new Person("David Hume"),
+            new Person("Rene Descartes"),
+            new Person("Thomas Aquinas")
+
+        };
+
+        Person[] sorted = parseContacts(arrayNames, "DESC");
+        assertThat(sorted).isEqualTo(expectedNames);
+    }
+    @Test
+    @DisplayName("2 Тест с курсa")
+    void test3() throws Exception {
+        String[] arrayNames = {"Paul Erdos", "Leonhard Euler", "Carl Gauss"};
+        Person[] expectedNames = {
+            new Person("Carl Gauss"),
+            new Person("Leonhard Euler"),
+            new Person("Paul Erdos"),
+        };
+
+        Person[] sorted = parseContacts(arrayNames, "DESC");
+        assertThat(sorted).isEqualTo(expectedNames);
+    }
+    @Test
+    @DisplayName("3 Тест с курсa")
+    void test4() throws Exception {
+        String[] arrayNames = {};
+        Person[] expectedNames = {};
+
+        Person[] sorted = parseContacts(arrayNames, "DESC");
+        assertThat(sorted).isEqualTo(expectedNames);
+    }
+    @Test
+    @DisplayName("4 Тест с курсa")
+    void test5() throws Exception {
+        Person[] expectedNames = {};
+
+        Person[] sorted = parseContacts(null, "DESC");
+        assertThat(sorted).isEqualTo(expectedNames);
+    }
+    @Test
+    @DisplayName("У части нет фамилий")
+    void test6() throws Exception {
+        String[] arrayNames = {"John", "Janette", "Linus Jackovich", "Wolf Jsonov"};
+        Person[] expectedNames = {
+            new Person("Linus Jackovich"),
+            new Person("Janette"),
+            new Person("John"),
+            new Person("Wolf Jsonov"),
+        };
+
+        Person[] sorted = parseContacts(arrayNames, "ABS");
+        assertThat(sorted).isEqualTo(expectedNames);
+    }
 }
