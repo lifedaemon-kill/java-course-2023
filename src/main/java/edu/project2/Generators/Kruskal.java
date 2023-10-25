c64dfa2f7431d0e2a099c127823abce7e80fac56
Ruslan <noname0263@gmail.com>
2023-10-25 20:02:30 +0700

    add files

---
+++ src/main/java/edu/project2/Generators/Kruskal.java
@@ -0,0 +1,13 @@
+package edu.project2.Generators;
+
+import edu.project2.Interfaces.Generator;
+import edu.project2.Utility.Cell;
+
+public class Kruskal implements Generator {
+    @Override
+    public Cell[][] generate(int height, int width, int seed) {
+        Cell[][] grid = new Cell[height][width];
+
+        return null;
+    }
+}
---
+++ src/main/java/edu/project2/Solvers/RightHand.java
@@ -0,0 +1,13 @@
+package edu.project2.Solvers;
+
+import edu.project2.Interfaces.Solver;
+import edu.project2.Utility.Cell;
+
+public class RightHand implements Solver {
+    private Cell[][] grid;
+
+    @Override
+    public Cell[][] solve(Cell[][] grid) {
+        return null;
+    }
+}
