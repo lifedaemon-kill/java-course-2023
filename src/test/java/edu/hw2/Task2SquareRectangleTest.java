2ab7b0f8048210be5120f3651a0ca6017c79d0a3
Ruslan <noname0263@gmail.com>
2023-10-26 14:27:05 +0700

    redirected Task2

--- src/test/java/edu/hw2/Task2SquareRectangleTest.java
+++ src/test/java/edu/hw2/Task2SquareRectangleTest.java
@@ -1,17 +1,17 @@
 package edu.hw2;
 
+import edu.hw2.Task2.Rectangle;
+import edu.hw2.Task2.Square;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-
-import static edu.hw2.Task2SquareRectangle.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class Task2SquareRectangleTest {
     static Arguments[] rectangles() {
-        return new Arguments[]{
+        return new Arguments[] {
             Arguments.of(new Rectangle()),
             Arguments.of(new Square())
         };
@@ -28,15 +28,17 @@ public class Task2SquareRectangleTest {
         assertThat(sqr.area()).isEqualTo(400.0);
 
     }
+
     @Test
-    @DisplayName("Площать квадрата, Default constructor")
+    @DisplayName("Площадь квадрата, Default constructor")
     void squareAreaDefault() {
         Rectangle sqr = new Square();
         sqr = sqr.setWidth(20);
         assertThat(sqr.area()).isEqualTo(400.0);
     }
+
     @Test
-    @DisplayName("Площать квадрата, Parametrized constructor")
+    @DisplayName("Площадь квадрата, Parametrized constructor")
     void squareAreaParameters() {
         Rectangle sqr = new Square(30);
         assertThat(sqr.area()).isEqualTo(900);
