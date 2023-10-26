eb49ce58e774c67f816f3d11cd83d6c32a6e6aba
Ruslan <noname0263@gmail.com>
2023-10-26 15:07:31 +0700

    update Utility

--- src/main/java/edu/hw2/Task3/Utility.java
+++ src/main/java/edu/hw2/Task3/Utility.java
@@ -8,7 +8,8 @@ public class Utility {
     }
 
     public static final Logger LOGGER = LogManager.getLogger();
-    public static final String FAULTY = "faulty";
     public static final String DROP_CONNECTION = "Can't create connection";
     public static final int DROP_RATE = 3;
+
+    public enum ManagersTypes { FAULTY, DEFAULT }
 }
