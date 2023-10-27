f8b77e46503b2acd87a6b855eb8785455d55c5fd
Ruslan <noname0263@gmail.com>
2023-10-27 16:11:28 +0700

    add bfs

---
+++ src/main/java/edu/project2/Solvers/BFS.java
@@ -0,0 +1,15 @@
+package edu.project2.Solvers;
+
+import edu.project2.Interfaces.Solver;
+import edu.project2.Utility;
+
+import static edu.project2.Utility.*;
+
+public class BFS implements Solver {
+    @Override
+    public Cell[][] solve(Cell[][] grid, int[] start, int[] end) {
+        Cell[][] solvedGrid = grid;
+
+        return solvedGrid;
+    }
+}
