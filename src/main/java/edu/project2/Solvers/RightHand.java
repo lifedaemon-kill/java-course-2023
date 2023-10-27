619c3cb0b2cfa9160fb40571c071d2bffe69a2d0
Ruslan <noname0263@gmail.com>
2023-10-27 16:11:52 +0700

    change parameters

--- src/main/java/edu/project2/Solvers/RightHand.java
+++ src/main/java/edu/project2/Solvers/RightHand.java
@@ -5,7 +5,7 @@ import edu.project2.Utility.Cell;
 
 public class RightHand implements Solver {
     @Override
-    public Cell[][] solve(Cell[][] grid) {
+    public Cell[][] solve(Cell[][] grid, int[] start, int[] end) {
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == Cell.PASSAGE) {
