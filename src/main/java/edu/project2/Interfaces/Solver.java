716c8f143fccf9a1eed61128d9f4287013e69dd9
Ruslan <noname0263@gmail.com>
2023-10-27 16:11:46 +0700

    change parameters

--- src/main/java/edu/project2/Interfaces/Solver.java
+++ src/main/java/edu/project2/Interfaces/Solver.java
@@ -3,5 +3,5 @@ package edu.project2.Interfaces;
 import edu.project2.Utility.Cell;
 
 public interface Solver {
-    Cell[][] solve(Cell[][] grid);
+    Cell[][] solve(Cell[][] grid, int[] start, int[] end);
 }
