2e17e6e5a7450f7f8a5d0648da2c94ba39bec9da
Ruslan <noname0263@gmail.com>
2023-10-29 16:25:30 +0700

    Test3 update to AAA + naming etc

--- src/test/java/edu/hw3/Task3ElementFrequencyTest.java
+++ src/test/java/edu/hw3/Task3ElementFrequencyTest.java
@@ -8,62 +8,74 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class Task3ElementFrequencyTest {
     @Test
-    @DisplayName("1 Тест с курса")
+    @DisplayName("Дважды повторяющиеся элементы")
     void test1() {
+        // Arrange
         Object[] request = {"a", "bb", "a", "bb"};
-
         HashMap<Object, Integer> expected = new HashMap<>() {{
             put("bb", 2);
             put("a", 2);
         }};
+        // Act
         HashMap<Object, Integer> answerArray = freqDict(request);
-
+        // Assert
         assertThat(answerArray).isEqualTo(expected);
     }
 
     @Test
-    @DisplayName("2 Тест с курса")
+    @DisplayName("Один элемент повторяется два раза")
     void test2() {
+        // Arrange
         Object[] request = {"this", "and", "that", "and"};
-
         HashMap<Object, Integer> expected = new HashMap<>() {{
             put("that", 1);
             put("and", 2);
             put("this", 1);
         }};
-
+        // Act
         HashMap<Object, Integer> answerArray = freqDict(request);
-
+        // Assert
         assertThat(answerArray).isEqualTo(expected);
     }
 
     @Test
-    @DisplayName("3 Тест с курса")
+    @DisplayName("Трижды повторяющийся элемент")
     void test3() {
+        // Arrange
         Object[] request = {"код", "код", "код", "bug"};
-
         HashMap<Object, Integer> expected = new HashMap<>() {{
             put("код", 3);
             put("bug", 1);
         }};
-
+        // Act
         HashMap<Object, Integer> answerArray = freqDict(request);
-
+        // Assert
         assertThat(answerArray).isEqualTo(expected);
     }
 
     @Test
-    @DisplayName("4 Тест с курса")
+    @DisplayName("Два элемента повторяются два раза")
     void test4() {
+        // Arrange
         Object[] request = {1, 1, 2, 2};
-
         HashMap<Object, Integer> expected = new HashMap<>() {{
             put(1, 2);
             put(2, 2);
         }};
-
+        // Act
         HashMap<Object, Integer> answerArray = freqDict(request);
-
+        // Assert
+        assertThat(answerArray).isEqualTo(expected);
+    }
+    @Test
+    @DisplayName("Отправляется пустой массив")
+    void test5() {
+        // Arrange
+        Object[] request = {};
+        HashMap<Object, Integer> expected = new HashMap<>();
+        // Act
+        HashMap<Object, Integer> answerArray = freqDict(request);
+        // Assert
         assertThat(answerArray).isEqualTo(expected);
     }
 }
