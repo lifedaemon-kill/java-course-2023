325c94e9713a96e9a1385794790be5cb68c7a84a
Ruslan <noname0263@gmail.com>
2023-10-29 20:21:29 +0700

    update Task8

--- src/test/java/edu/hw3/Task8BackwardIteratorTest.java
+++ src/test/java/edu/hw3/Task8BackwardIteratorTest.java
@@ -10,35 +10,37 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class Task8BackwardIteratorTest {
     @Test
-    @DisplayName("List.of")
+    @DisplayName("Is List.of backward-iterating correct")
     void test1() {
+        // Arrange
         int[] result = new int[3];
+        int[] expected = {3, 2, 1};
 
+        // Act
         var iterator = new BackwardIterator<>(List.of(1, 2, 3));
-
         for (int i = 0; iterator.hasNext(); i++) {
             result[i] = iterator.next();
         }
 
-        int[] expected = {3, 2, 1};
-
+        // Assert
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    @DisplayName("Arrays.asList")
+    @DisplayName("Is Arrays.asList backward-iterating correct")
     void test2() {
+        // Arrange
         ArrayList<Integer> result = new ArrayList<>();
         ArrayList<Integer> target = new ArrayList<>(Arrays.asList(1, 2, 3));
+        ArrayList<Integer> expected = new ArrayList<>(Arrays.asList(3, 2, 1));
 
+        // Act
         var iterator = new BackwardIterator<>(target);
-
         while (iterator.hasNext()) {
             result.add(iterator.next());
         }
 
-        ArrayList<Integer> expected = new ArrayList<>(Arrays.asList(3, 2, 1));
-
+        // Assert
         assertThat(result).isEqualTo(expected);
     }
 }
