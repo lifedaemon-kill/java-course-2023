7243a7a16caeb5bb2a004af1b64346a2936a22f4
Ruslan <noname0263@gmail.com>
2023-10-30 14:31:57 +0700

    add Task1, Task2, Task3

--- src/main/java/edu/hw4/AnimalsData.java
+++ src/main/java/edu/hw4/AnimalsData.java
@@ -2,16 +2,32 @@ package edu.hw4;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class AnimalsData {
-    private AnimalsData (){}
+    private AnimalsData() {
+    }
 
     //Задача 1 отсортировать животных по росту от самого маленького к самому большому -> List<Animal>
-    public static List<Animal> getSortedAnimalsByHeight(List<Animal> animals){
+    public static List<Animal> getSortedAnimalsByHeight(List<Animal> animals) {
         return animals.stream()
             .sorted(Comparator.comparingInt(Animal::height))
             .collect(Collectors.toList());
+    }
+
+    //Задача 2 Отсортировать животных по весу от самого тяжелого к самому легкому, выбрать k первых -> List<Animal>
+    public static List<Animal> getSortedAnimalsByWeightDesc(List<Animal> animals, int k) {
+        return animals.stream()
+            .sorted(Comparator.comparingInt(Animal::weight).reversed())
+            .limit(k)
+            .collect(Collectors.toList());
+    }
+
+    //Задача 3 Сколько животных каждого вида -> Map<Animal.Type, Integer>
+    public static Map<Animal.Type, Integer> getAnimalTypeMap(List<Animal> animals){
+        return animals.stream()
+            .collect(Collectors.groupingBy(Animal::type, Collectors.summingInt(animal -> 1)));
     }
 
 }
---
+++ src/test/java/edu/hw4/Task2Test.java
@@ -0,0 +1,85 @@
+package edu.hw4;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Task2Test {
+    @Test
+    @DisplayName("Сортировка животных по весу, К = длине массива")
+    void test1() {
+        // Arrange
+        List<Animal> animals = new ArrayList<>();
+        animals.add(new Animal("Мистер кот", Animal.Type.CAT,     Animal.Sex.M, 5,     24,   3, true));
+        animals.add(new Animal("Мистер паук", Animal.Type.SPIDER, Animal.Sex.M, 1,     1,    0, true));
+        animals.add(new Animal("Джон", Animal.Type.DOG,           Animal.Sex.M, 5,     40,   15, true));
+        animals.add(new Animal("Королева рыб", Animal.Type.FISH,  Animal.Sex.F, 3,     10,   5, false));
+        animals.add(new Animal("Птица феникс", Animal.Type.BIRD,  Animal.Sex.F, 10000, 1500, 700, false));
+
+        List<Animal> expected = new ArrayList<>();
+        expected.add(animals.get(4));
+        expected.add(animals.get(2));
+        expected.add(animals.get(3));
+        expected.add(animals.get(0));
+        expected.add(animals.get(1));
+
+        // Act
+        List<Animal> result = AnimalsData.getSortedAnimalsByWeightDesc(animals, animals.size());
+
+        //Assert
+        assertThat(result).isEqualTo(expected);
+    }
+    @Test
+    @DisplayName("Сортировка животных по весу, К = половине длине массива")
+    void test3() {
+        // Arrange
+        List<Animal> animals = new ArrayList<>();
+        animals.add(new Animal("Мистер кот", Animal.Type.CAT,     Animal.Sex.M, 5,     24,   3, true));
+        animals.add(new Animal("Мистер паук", Animal.Type.SPIDER, Animal.Sex.M, 1,     1,    0, true));
+        animals.add(new Animal("Джон", Animal.Type.DOG,           Animal.Sex.M, 5,     40,   15, true));
+        animals.add(new Animal("Королева рыб", Animal.Type.FISH,  Animal.Sex.F, 3,     10,   5, false));
+        animals.add(new Animal("Птица феникс", Animal.Type.BIRD,  Animal.Sex.F, 10000, 1500, 700, false));
+
+        List<Animal> expected = new ArrayList<>();
+        expected.add(animals.get(4));
+        expected.add(animals.get(2));
+
+        // Act
+        List<Animal> result = AnimalsData.getSortedAnimalsByWeightDesc(animals, animals.size() / 2);
+
+        //Assert
+        assertThat(result).isEqualTo(expected);
+    }
+    @Test
+    @DisplayName("Сортировка животных по весу, К = 0")
+    void test4() {
+        // Arrange
+        List<Animal> animals = new ArrayList<>();
+        animals.add(new Animal("Мистер кот", Animal.Type.CAT,     Animal.Sex.M, 5,     24,   3, true));
+        animals.add(new Animal("Мистер паук", Animal.Type.SPIDER, Animal.Sex.M, 1,     1,    0, true));
+        animals.add(new Animal("Джон", Animal.Type.DOG,           Animal.Sex.M, 5,     40,   15, true));
+        animals.add(new Animal("Королева рыб", Animal.Type.FISH,  Animal.Sex.F, 3,     10,   5, false));
+        animals.add(new Animal("Птица феникс", Animal.Type.BIRD,  Animal.Sex.F, 10000, 1500, 700, false));
+
+        List<Animal> expected = new ArrayList<>();
+
+        // Act
+        List<Animal> result = AnimalsData.getSortedAnimalsByWeightDesc(animals, 0);
+
+        //Assert
+        assertThat(result).isEqualTo(expected);
+    }
+    @Test
+    @DisplayName("Пустой массив")
+    void test2() {
+        // Arrange
+        List<Animal> animals = new ArrayList<>();
+        List<Animal> expected = new ArrayList<>();
+        // Act
+        List<Animal> result = AnimalsData.getSortedAnimalsByWeightDesc(animals, animals.size());
+        //Assert
+        assertThat(result).isEqualTo(expected);
+    }
+}
