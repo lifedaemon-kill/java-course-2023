f1f20a40ccc4eaba07dea4335394ac77ab93ade5
Ruslan <noname0263@gmail.com>
2023-11-12 01:54:17 +0700

    add file

---
+++ src/main/java/edu/hw5/Task1/AverageTime.java
@@ -0,0 +1,12 @@
+package edu.hw5.Task1;
+
+public class AverageTime {
+    private AverageTime() {
+    }
+    /*
+    public static Instant averageTime(List<String> durations){
+        //List<>
+    }
+
+     */
+}
